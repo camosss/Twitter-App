e157467cc76e715bf37a27af1f5c8e358750d845
camosss <camosss@naver.com>
2021-06-11 17:18:50 +0900

    Upload Follow & Reply Notification

--- TwitterTutorial/API/NotificationService.swift
+++ TwitterTutorial/API/NotificationService.swift
@@ -9,8 +9,10 @@ import Firebase
 
 struct NotificationService {
     static let shared = NotificationService()
-                                                    // 여기에 트윗을 전달
-    func uploadNotification(type: NotificationType, tweet: Tweet? = nil) {
+    
+    func uploadNotification(type: NotificationType,
+                            tweet: Tweet? = nil,// 여기에 트윗을 전달
+                            user: User? = nil) {// 알림을 보낼 때 특정사용자에게 후속 알림을 보내야하기 때문에
         guard let uid = Auth.auth().currentUser?.uid else { return }
         
         var values: [String: Any] = ["timestamp": Int(NSDate().timeIntervalSince1970),
@@ -20,8 +22,8 @@ struct NotificationService {
         if let tweet = tweet {
             values["tweetID"] = tweet.tweetID
             REF_NOTIFICATIONS.child(tweet.user.uid).childByAutoId().updateChildValues(values)
-        } else {
-            
+        } else if let user = user {
+            REF_NOTIFICATIONS.child(user.uid).childByAutoId().updateChildValues(values)
         }
     }
 }
